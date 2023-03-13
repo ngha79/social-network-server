66a52404e1426cafd79ee5f3d300f88456cac837
ngha79 <nguyenmha07092003@gmail.com>
2023-03-13 15:56:37 +0700

    change

--- api/middlewares/checkAuth.js
+++ api/middlewares/checkAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const checkAuth = (req, res, next) => {
   try {
-    const token = req.header("Authorization").split(" ")[1];
+    const token = req.header("Authorization").replace("Bearer", "");
   } catch (error) {
     res.status(401).json({
       status: 401,
